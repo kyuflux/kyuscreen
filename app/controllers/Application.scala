79166361f7fe01953b965cbc2da62007a87d7a70
= <=>
2015-05-16 10:59:24 -0300

    actor bound

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -26,28 +26,67 @@ object Application extends Controller {
 				counter += 1
 				counter.toString
 			}
+			Akka.system.actorOf(Props[CorporateActor],corporate) ! Child(branch)
+			Akka.system.actorSelection(s"/user/$corporate/$branch") ! Child(screen)
 			Ok(views.html.index("Your new application is ready."))
 				.withSession(request.session + (UID -> uid))
 	}
 
+	def send(corporate:String, branch:String, screen:String,msg:String) = Action {
+		Akka.system.actorSelection(s"/user/$corporate/$branch/$screen") ! JsObject(Seq(
+			"mensaje" -> JsString(msg)))
+		Ok(s"sent $msg!")
+	} 
+
 	def ws(corporate:String, branch:String, screen:String) = 
 		WebSocket.tryAcceptWithActor[JsValue, JsValue] { implicit request =>
-		Future.successful(request.session.get(UID) match {
-			case None => Left(Forbidden)
-			case Some(uid) => Right(WebSocketActor.props(uid))
-		})
+
+			val scrn = Akka.system.actorSelection(s"/user/$corporate/$branch/$screen")
+			Future.successful(request.session.get(UID) match {
+				case None => Left(Forbidden)
+				case Some(uid) => Right(WebSocketActor.props(uid,scrn))
+			})
+		}
+}
+
+
+
+class CorporateActor extends Actor{
+	def receive = {
+		case Child(name) => context.actorOf(Props[BranchActor], name)
+	}
+}
+class BranchActor extends Actor{
+	def receive = {
+		case Child(name) => context.actorOf(Props[ScreenActor], name)
+	}
+}
+class ScreenActor extends Actor{
+	val log = Logging(context.system,this)
+	var wsClients = Set[ActorRef]()
+	def receive = {
+		case Subscribe => 
+			wsClients += sender()
+			//context watch sender
+			log.info("subscribing")
+		case msg:JsValue => 
+			log.info(msg.toString)
+			wsClients foreach {_ ! msg }
 	}
 }
 
-object WebSocketActor{
-	def props(uid:String)(out:ActorRef) = Props(new WebSocketActor(uid,out))
-}
-class WebSocketActor(uid:String, out:ActorRef) extends Actor{
-	val log = Logging(context.system,this)
-	def receive = {
-		case msg:JsValue => 
-			log.info( (msg \ "name").as[String] ) 
-			out ! msg 
-	}
-}
+		case class Child(name:String)
+		object Subscribe
+		object WebSocketActor{
+			def props(uid:String,screen:ActorSelection)(out:ActorRef) = Props(new WebSocketActor(uid,out,screen))
+		}
+		class WebSocketActor(uid:String, out:ActorRef, screen:ActorSelection) extends Actor{
+			override def preStart() = screen ! Subscribe 
+			val log = Logging(context.system,this)
+			def receive = {
+				case msg:JsValue => 
+					log.info( msg toString ) 
+					out ! msg 
+			}
+		}
 
